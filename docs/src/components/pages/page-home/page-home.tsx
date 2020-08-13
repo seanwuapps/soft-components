881fcd5666b5d74caeaad4e405f2ac9445f772d8
Sean Wu <seanwuapps@gmail.com>
2020-08-13 22:33:58 +1000

    home

--- docs/src/components/pages/page-home/page-home.tsx
+++ docs/src/components/pages/page-home/page-home.tsx
@@ -8,14 +8,15 @@ export class PageHome {
   render() {
     return (
       <Host>
-        <h1>Home page</h1>
-        <div class="row">
-          <div class="col-xs">auto</div>
-          <div class="col-xs">auto</div>
-          <div class="col-xs">auto</div>
+        <div class="container">
+          <div class="flex">
+            <h1 class="w-5">Soft components</h1>
+            <p class="w-5 ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic magnam dolorem, quidem ex ducimus consequatur molestiae sunt est eveniet placeat porro sint excepturi
+              voluptatum aperiam maxime id alias, sit deleniti?
+            </p>
+          </div>
         </div>
-        <sc-button>TEST</sc-button>
-        <slot></slot>
       </Host>
     );
   }
