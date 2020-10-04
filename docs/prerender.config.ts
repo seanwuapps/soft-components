8d603d8c6fc14ad4b6fec2a3935501aa3f985ad4
Sean Wu <seanwuapps@gmail.com>
2020-10-04 19:02:19 +1100

    docs styling update

--- docs/prerender.config.ts
+++ docs/prerender.config.ts
@@ -6,7 +6,7 @@ const softComponents = docsData.components.map(component => component.tag);
 export const config: PrerenderConfig = {
   hydrateOptions(url) {
     return {
-      excludeComponents: [...softComponents, 'linkable-title', 'codpen-link'],
+      excludeComponents: [...softComponents, 'linkable-title', 'codpen-link', 'seanwu-logo'],
     };
   },
 };
