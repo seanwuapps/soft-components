51c0350995284618911e7b5cd43f11455392c938
Sean Wu <seanwuapps@gmail.com>
2020-11-22 21:39:38 +1100

    indeterminate circular animation

--- src/components/sc-progress/sc-progress.tsx
+++ src/components/sc-progress/sc-progress.tsx
@@ -28,26 +28,66 @@ export class ScProgress {
 
   circleEl = null
 
+  spinnerAnimationId = null
+
+  startIndeterminateAnimation() {
+    let upper = 1000
+    let lower = 0
+    let dashoffset = upper
+
+    let goingDown = true
+    let speed = 1 //
+    const frame = () => {
+      if (dashoffset < lower) {
+        goingDown = false
+      }
+
+      if (dashoffset > upper) {
+        goingDown = true
+      }
+      if (goingDown) {
+        dashoffset -= speed
+      } else {
+        dashoffset += speed
+      }
+
+      console.log(dashoffset)
+      this.circleEl.style.cssText = 'stroke-dashoffset: ' + dashoffset
+      // requestAnimationFrame(frame)
+    }
+    this.spinnerAnimationId = requestAnimationFrame(frame)
+  }
+
+  disconnectedCallback() {
+    if (this.spinnerAnimationId) {
+      cancelAnimationFrame(this.spinnerAnimationId)
+    }
+  }
+
   renderCircular(size) {
-    
+    // commented codes are attempts to do gapped circular progress bar.
     // const gapPercent = 0
     // const offsetFactor = (100 - gapPercent) / 100
     // const max = offsetFactor * (size * Math.PI) // c
-
-    const max = size * Math.PI
-    const offset = (1 - this.percentage / 100) * max
-    const strokeWidth = size / 10
-    
     /**
      * -270: rotate starting point to bottom center
      */
     // const rotate = -270 + (360 * gapPercent) / 100 / 2
+
+    if (this.indeterminate) {
+      // this.startIndeterminateAnimation()
+    }
+
+    const max = size * Math.PI
+    const offset = (1 - this.percentage / 100) * max
+    const strokeWidth = size / 10
 
     return (
       <div
         class="circular-container"
         style={{
           '--sc-progress-circular-size': `${size}px`,
+          '--sc-progress-circular-size-number': `${size}`,
           '--sc-progress-stroke-width': `${strokeWidth}px`,
           // '--sc-progress-circular-initial-rotate': `${rotate}deg`
         }}
@@ -61,13 +101,15 @@ export class ScProgress {
             cx="50%"
             cy="50%"
             style={{
-              '--sc-progress-circular-stroke-dasharray': `${max} ${Number.MAX_SAFE_INTEGER}`,
+              '--sc-progress-circular-stroke-dasharray': `${max} ${max * 2}`,
               '--sc-progress-circular-stroke-dashoffset': `${offset}`,
             }}
           />
         </svg>
         {/* label display */}
-        <div class="label">{this.percentage}%</div>
+        <div class="label">
+          {this.percentage !== null ? this.percentage + '%' : ''}
+        </div>
       </div>
     )
   }
